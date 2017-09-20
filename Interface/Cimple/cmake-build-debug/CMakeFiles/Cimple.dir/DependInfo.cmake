93caa28b2d79041a283481196a8b15e69728f573
jfeldstein <jfeldstein@caltech.edu>
2017-09-20 13:11:56 -0700

    Added find_control library: calculates needed input, system: contains all definitions of different structs, main.c contains an example executable. cmake files help compiling projects. Status: Project is compilable main functions runs correctly, find_control needs to be checked for dimension errors

---
+++ Interface/Cimple/cmake-build-debug/CMakeFiles/Cimple.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/be107admin/C-TuLiP/Interface/Cimple/find_controller.c" "/home/be107admin/C-TuLiP/Interface/Cimple/cmake-build-debug/CMakeFiles/Cimple.dir/find_controller.c.o"
+  "/home/be107admin/C-TuLiP/Interface/Cimple/main.c" "/home/be107admin/C-TuLiP/Interface/Cimple/cmake-build-debug/CMakeFiles/Cimple.dir/main.c.o"
+  "/home/be107admin/C-TuLiP/Interface/Cimple/system.c" "/home/be107admin/C-TuLiP/Interface/Cimple/cmake-build-debug/CMakeFiles/Cimple.dir/system.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/python2.7"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
