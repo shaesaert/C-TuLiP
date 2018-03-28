f020d050cc213d5785f2989ca83d4ff2c1225541
jfeldstein <jfeldstein@caltech.edu>
2018-03-27 19:08:04 -0700

    First step to safe mode

--- Interface/Cimple/cimple_safe_mode.h
+++ Interface/Cimple/cimple_safe_mode.h
@@ -5,4 +5,38 @@
 #ifndef CIMPLE_CIMPLE_SAFE_MODE_H
 #define CIMPLE_CIMPLE_SAFE_MODE_H
 
+#include "cimple_controller.h"
+
+
+int check_backup(gsl_vector *x_real,
+                 gsl_vector *u,
+                 gsl_matrix *A,
+                 gsl_matrix *B,
+                 polytope *check_polytope);
+
+polytope* previous_polytope(polytope *P1,
+                            polytope *P2,
+                            system_dynamics *s_dyn);
+
+void safe_mode_polytopes(system_dynamics *s_dyn,
+                         polytope *current_polytope,
+                         polytope *safe_polytope,
+                         size_t time_horizon,
+                         polytope **polytope_list_safemode);
+
+void next_safemode_input(gsl_matrix *u,
+                         current_state *now,
+                         polytope *current,
+                         polytope *next,
+                         system_dynamics *s_dyn,
+                         cost_function *f_cost);
+
+void *next_safemode_computation(void *arg);
+
+void * total_safe_mode_computation(void *arg);
+
+poly_t * solve_feasible_closed_loop(poly_t *p_universe,
+                                    polytope *P1,
+                                    polytope *P2,
+                                    system_dynamics *s_dyn);
 #endif //CIMPLE_CIMPLE_SAFE_MODE_H
