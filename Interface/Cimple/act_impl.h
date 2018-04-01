e0fded9a2833e92231bea6a5ef3bb1b7189db8b8
jfeldstein <jfeldstein@caltech.edu>
2018-04-01 10:15:39 -0700

    Emptied act_impl files

--- Interface/Cimple/act_impl.h
+++ Interface/Cimple/act_impl.h
@@ -8,12 +8,12 @@
 #include <stdio.h>
 #include "cimple_controller.h"
 
-void ACT_m1(current_state * now, discrete_dynamics * d_dyn, system_dynamics * s_dyn, cost_function * f_cost);
-
-void ACT_m0(current_state * now, discrete_dynamics * d_dyn, system_dynamics * s_dyn, cost_function * f_cost);
-
-void ACT_m2(current_state * now, discrete_dynamics * d_dyn, system_dynamics * s_dyn, cost_function * f_cost);
-
-void ACT_m3(current_state * now, discrete_dynamics * d_dyn, system_dynamics * s_dyn, cost_function * f_cost);
+//void ACT_m1(current_state * now, discrete_dynamics * d_dyn, system_dynamics * s_dyn, cost_function * f_cost);
+//
+//void ACT_m0(current_state * now, discrete_dynamics * d_dyn, system_dynamics * s_dyn, cost_function * f_cost);
+//
+//void ACT_m2(current_state * now, discrete_dynamics * d_dyn, system_dynamics * s_dyn, cost_function * f_cost);
+//
+//void ACT_m3(current_state * now, discrete_dynamics * d_dyn, system_dynamics * s_dyn, cost_function * f_cost);
 
 #endif //CIMPLE_ACT_IMPL_H
